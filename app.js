4848b275a74022ab7b092904a74c01cfc1dc713e
jose torres <jtorres.pdx@gmail.com>
2015-10-06 16:24:56 -0700

    add home.html and its router

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var flix = angular.module('flix', ['ui.router']);
 
-flix.config(function($stateProvider) {
+flix.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
     url:'',
     views:{
       'header': { templateUrl: 'partials/header.html' },
-      // 'body': {
+      // 'home': {
       //   templateUrl: 'partials/home.html',
       // }
     }
